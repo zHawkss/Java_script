b7b6d905bda379a8f4813411f0570deaad21bf61
Kevin Barcellos <kevinr.barcellos@gmail.com>
2023-01-18 20:45:21 -0300

    Exercicio_aula2

---
+++ script.js
@@ -0,0 +1,39 @@
+// a. Márcia ganhou 7 pacotes com 30 adesivos em cada pacote. 15 adesivos eram repetidos e não puderam ser colados no álbum. Quantos adesivos Márcia colou no álbum?
+
+{
+let pacotes = 7;
+let adesivos = 30;
+let colados = (pacotes * adesivos) -15 ;
+console.log ('Adesivos colado', colados);
+}
+
+//Numa caixa de refrigerantes cabem duas dúzias de latas. Quantas latas poderão ser guardadas em 4 caixas?
+
+{
+    let latas = 24;
+    let caixas = 4; 
+    let resultado = latas * caixas;
+    console.log('Quantidade de latas em 4 caixas', resultado);
+}
+
+
+//Utilizando decremento escreva no console todos os números ímpar no intervalo de 10 até 0.
+
+{
+let valor = 10;
+         valor--;
+         console.log("valor ímpar: ", valor);
+         valor--;
+         valor--;
+         console.log("valor ímpar: ", valor);
+         valor--;
+         valor--;
+         console.log("valor ímpar: ", valor);
+         valor--;
+         valor--;
+         console.log("valor ímpar: ", valor);
+         valor--;
+         valor--;
+         console.log("valor ímpar: ", valor);
+
+        }
