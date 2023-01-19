2b33bbc07afb393fe2cd232430778243d82d770b
Kevin Barcellos <kevinr.barcellos@gmail.com>
2023-01-19 20:50:18 -0300

    duas pastas

---
+++ aula3- pratica/script.js
@@ -0,0 +1,43 @@
+//Crie uma variável e armazene a seguinte string “Manipulando strings com JS”
+{
+let manipuador = 'Manipulando strings'
+
+console.log(manipuador)
+}
+
+
+
+{
+let textoMaiusculo = 'textograndao'
+
+console.log(textoMaiusculo .toUpperCase())
+}
+
+{
+        let primeirasstrings = 'Qualquer coisa aleatoria'
+        console.log(primeirasstrings .slice(0,5))
+}
+
+//d. Exiba em um alerta a quantidade de caracteres que contém esta string.
+
+{
+ let alerta = 'Aqui existem alguns caracteres'
+ alert( alerta .length)
+}
+
+//Crie uma variável e armazene a seguinte string “Vamos concatenar as duas strings”.
+
+
+let stringarmazenar = 'Vamos concatenar as duas strings'
+console.log(stringarmazenar)
+
+
+
+
+{
+let armazenar = 'duas mensagens em'
+armazenar = `${armazenar} duas strings`
+
+console.log('contactenando:', armazenar)
+
+}
